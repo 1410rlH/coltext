516451e78659d417b8839192b0d018040b26465c
earl <earl@laptop.domain.name>
2020-02-29 20:25:02 -0500

    Coltext 1.0.0 version (with tests).

---
+++ coltext.hpp
@@ -0,0 +1,525 @@
+// Coltext. Text's color and style manipulator.
+
+// Copyright (C) 2020 by Earl H. (1410rlH)
+//
+// This library is created to be a convenient tool 
+// for manipulations with text's color and style.
+// 
+// The main feature of Coltext is a special symbol '#'
+// used for in-string ANSI effect parsing, which allows 
+// programmers not to care about resetting streams.
+
+// This is header only library.
+
+//
+// Distributed by the terms of GPL. 
+// See LICENSE for details.
+//
+
+#ifndef COLTEXT_HPP
+#define COLTEXT_HPP "1.0.0"
+
+
+#include <iostream>
+
+#include <list>
+#include <stack>
+#include <unordered_map>
+
+
+/** 
+ * @class Coltext
+ * @brief:
+ *  Colored text.
+ *  
+ *  In order to use Coltext ANSI features 
+ *  you need to convert strings to it.
+ */ 
+class Coltext {
+public:
+    inline Coltext();
+    inline Coltext(const std::string &);
+    inline Coltext(const char *, size_t );
+
+    Coltext & operator+= (const Coltext &);
+    
+    friend inline Coltext & operator+  (const Coltext &, const Coltext &);
+
+    friend inline std::istream & operator>> (std::istream &, Coltext &);
+    friend inline std::ostream & operator<< (std::ostream &, const Coltext &);
+
+private:
+    struct Token {
+        enum class Type {
+            text,       // plain text
+            effect,     // #effect(
+            effect_stop // )#
+        };
+
+        Type type;
+        std::string value;
+    };
+    std::list<Token> tokenize(const char *str, size_t len) const;
+
+    struct Node {
+        enum class Type {
+            text,
+            effect
+        };
+
+        Type type;
+        std::string value;
+    };
+    std::list<Node> parse(const std::list<Token> &tokens) const;
+
+
+    std::string str;
+    std::string colored_str;
+};
+
+inline namespace literals {
+    Coltext operator"" _col(const char *str, size_t len);
+};
+
+namespace ansi {
+
+/** 
+ * @enum class Effect
+ * @brief:
+ *  SGR (Set Graphic Rendition) codes.
+ *  Enum of all common ANSI effects that Coltext supprorts.
+ * @see: https://en.wikipedia.org/wiki/ANSI_escape_code;
+ */ 
+enum class Effect {
+    reset     = 0, // Reset all effects
+
+/* Foreground (text) style */
+    bold      = 1,
+    faint     = 2,
+    italic    = 3,
+    underline = 4,
+    blink     = 5,
+
+    reverse   = 7, // Reverse foreground and background colors
+
+    crossed   = 9,
+
+    double_underline = 21, 
+
+/* Turn off foreground effect */
+    normal_itensity = 22, // Bold and faint off
+
+    italic_off    = 23,
+    underline_off = 24,
+    blink_off     = 25,
+
+    reverse_off   = 27,
+
+    crossed_off   = 29,
+
+
+/* Foreground color */
+    black_fg   = 30,
+    red_fg     = 31,
+    green_fg   = 32,
+    yellow_fg  = 33,
+    blue_fg    = 34,
+    magenta_fg = 35,
+    cyan_fg    = 36,
+    white_fg   = 37,
+
+#ifdef BLUSH_ANSI_RGB
+    rgb_fg     = 38,
+#endif
+    default_fg = 39,
+
+/* Background color */
+    black_bg   = 40,
+    red_bg     = 41,
+    green_bg   = 42,
+    yellow_bg  = 43,
+    blue_bg    = 44,
+    magenta_bg = 45,
+    cyan_bg    = 46,
+    white_bg   = 47,
+
+#ifdef BLUSH_ANSI_RGB
+    rgb_bg     = 48,
+#endif
+    default_bg = 49,
+
+/* Frame styles */
+ 	framed     = 51, 	
+ 	encircled  = 52,
+ 	overlined  = 53, 	
+
+/* Turn off frame style */
+ 	framed_off     = 54, // Framed and encircled off	
+ 	overlined_off = 55, 
+
+/* Bright colors */
+    bright_black_fg   = 90,
+    bright_red_fg     = 91,
+    bright_green_fg   = 92,
+    bright_yellow_fg  = 93,
+    bright_blue_fg    = 94,
+    bright_magenta_fg = 95,
+    bright_cyan_fg    = 96,
+    bright_white_fg   = 97,
+
+    bright_black_bg   = 100,
+    bright_red_bg     = 101,
+    bright_green_bg   = 102,
+    bright_yellow_bg  = 103,
+    bright_blue_bg    = 104,
+    bright_magenta_bg = 105,
+    bright_cyan_bg    = 106,
+    bright_white_bg   = 107
+};
+
+/* Map of opposit effects. Colors can not be canceled like this.*/
+const
+std::unordered_map<Effect, Effect> effect_off = {
+    {Effect::bold, Effect::normal_itensity}, {Effect::faint, Effect::normal_itensity},
+     
+    {Effect::underline, Effect::underline_off}, {Effect::double_underline, Effect::underline_off},
+    {Effect::framed, Effect::framed_off}, {Effect::encircled, Effect::framed_off},
+    
+    {Effect::italic, Effect::italic_off}, 
+    {Effect::blink, Effect::blink_off},
+    {Effect::reverse, Effect::reverse_off},
+    {Effect::crossed, Effect::crossed_off},
+    {Effect::overlined, Effect::overlined_off}
+};
+
+/* Map of supported effect acronyms. Format: "#name:" or "#name(". */
+const
+std::unordered_map<std::string, Effect> name_to_effect = {
+/* HTML tags used as acronyms for styles */
+    {"bold",      Effect::bold},      {"<b>", Effect::bold},
+    {"faint",     Effect::faint},     {"<f>", Effect::faint},
+    {"italic",    Effect::italic},    {"<i>", Effect::italic},
+    {"underline", Effect::underline}, {"<u>", Effect::underline},
+
+    // <s> and <del> are HTML tags. <c> is used to follow the pattern with first letter.
+    {"crossed", Effect::crossed}, {"<c>", Effect::crossed}, {"<s>", Effect::crossed}, {"<del>", Effect::crossed},
+    
+/* Not frequently used functions are without acronyms. */
+    {"blink",   Effect::blink}, 
+    {"reverse", Effect::reverse},
+
+
+/* RGB and CMYK used as acronyms for colors */
+    {"black",   Effect::black_fg},   {"k", Effect::black_fg},
+    {"red",     Effect::red_fg},     {"r", Effect::red_fg},
+    {"green",   Effect::green_fg},   {"g", Effect::green_fg},
+    {"yellow",  Effect::yellow_fg},  {"y", Effect::yellow_fg},
+    {"blue",    Effect::blue_fg},    {"b", Effect::blue_fg},
+    {"magenta", Effect::magenta_fg}, {"m", Effect::magenta_fg},
+    {"cyan",    Effect::cyan_fg},    {"c", Effect::cyan_fg},
+    {"white",   Effect::white_fg},   {"w", Effect::white_fg},
+
+    {"Black",   Effect::black_bg},   {"K", Effect::black_bg},
+    {"Red",     Effect::red_bg},     {"R", Effect::red_bg},
+    {"Green",   Effect::green_bg},   {"G", Effect::green_bg},
+    {"Yellow",  Effect::yellow_bg},  {"Y", Effect::yellow_bg},
+    {"Blue",    Effect::blue_bg},    {"B", Effect::blue_bg},
+    {"Magenta", Effect::magenta_bg}, {"M", Effect::magenta_bg},
+    {"Cyan",    Effect::cyan_bg},    {"C", Effect::cyan_bg},
+    {"White",   Effect::white_bg},   {"W", Effect::white_bg},
+
+    {"bright_black",   Effect::bright_black_fg},   {"bk", Effect::bright_black_fg},
+    {"bright_red",     Effect::bright_red_fg},     {"br", Effect::bright_red_fg},
+    {"bright_green",   Effect::bright_green_fg},   {"bg", Effect::bright_green_fg},
+    {"bright_yellow",  Effect::bright_yellow_fg},  {"by", Effect::bright_yellow_fg},
+    {"bright_blue",    Effect::bright_blue_fg},    {"bb", Effect::bright_blue_fg},
+    {"bright_magenta", Effect::bright_magenta_fg}, {"bm", Effect::bright_magenta_fg},
+    {"bright_cyan",    Effect::bright_cyan_fg},    {"bc", Effect::bright_cyan_fg},
+    {"bright_white",   Effect::bright_white_fg},   {"bw", Effect::bright_white_fg},
+
+    {"bright_Black",   Effect::bright_black_bg},   {"bK", Effect::bright_black_bg}, 
+    {"bright_Red",     Effect::bright_red_bg},     {"bR", Effect::bright_red_bg},
+    {"bright_Green",   Effect::bright_green_bg},   {"bG", Effect::bright_green_bg},
+    {"bright_Yellow",  Effect::bright_yellow_bg},  {"bY", Effect::bright_yellow_bg},
+    {"bright_Blue",    Effect::bright_blue_bg},    {"bB", Effect::bright_blue_bg},
+    {"bright_Magenta", Effect::bright_magenta_bg}, {"bM", Effect::bright_magenta_bg},
+    {"bright_Cyan",    Effect::bright_cyan_bg},    {"bC", Effect::bright_cyan_bg},
+    {"bright_White",   Effect::bright_white_bg},   {"bW", Effect::bright_white_bg}
+};
+
+} // inline namespace ansi
+
+
+inline Coltext::Coltext() 
+: str(""),
+  colored_str("")
+{};
+
+inline Coltext::Coltext(const std::string &str) 
+{
+    this->str = str;
+    auto tokens = this->tokenize(str.c_str(), str.size());
+    auto nodes = this->parse(tokens);
+    for (const auto &node : nodes) this->colored_str += node.value;
+};
+
+inline Coltext::Coltext(const char *str, size_t len)
+{
+    this->str = std::string(str, len);
+    auto tokens = this->tokenize(str, len);
+    auto nodes = this->parse(tokens);
+    for (const auto &node : nodes) this->colored_str += node.value;
+}
+
+Coltext & Coltext::operator+= (const Coltext &rhs)
+{
+    this->str += rhs.str;
+    auto tokens = this->tokenize(this->str.c_str(), this->str.size());
+    auto nodes = this->parse(tokens);
+
+    this->colored_str = "";
+    for (const auto &node : nodes) this->colored_str += node.value;
+
+    return *this;
+}
+
+inline std::istream & operator>> (std::istream &is, Coltext &ctxt)
+{
+    std::string str; std::getline(is, str);
+
+    ctxt.str = str;
+    auto tokens = ctxt.tokenize(str.c_str(), str.size());
+    auto nodes = ctxt.parse(tokens);
+    for (const auto &node : nodes) ctxt.colored_str += node.value;
+    
+    return is;
+}
+
+inline std::ostream & operator<< (std::ostream &os, const Coltext &ctxt)
+{
+    return os << ctxt.colored_str;
+}
+
+Coltext literals::operator"" _col(const char *str, size_t len)
+{
+    return Coltext(str, len);
+}
+
+std::list<Coltext::Token> 
+Coltext::tokenize (const char *str, size_t len) const
+{
+    std::list<Token> tokens;
+    
+    bool wait_next_word = false;
+    int wait_right_parenthesis = 0;
+
+    std::string buffer = "";
+    for (size_t i = 0; i < len; ++i)
+    {  
+        char c = str[i];
+        if (c == '\\' && i + 1 < len && 
+            (str[i+1] == '(' || str[i+1] == ')' || str[i+1] == '#'))
+        { 
+        /* If parentheses are escaped,
+           push them without slash */
+            ++i;
+            buffer.push_back(str[i+1]);
+            continue;
+        }
+        else
+        if (c == '#' || c == '<')
+        {/* Coltext tags found */
+            
+            tokens.push_back({ 
+                Token::Type::text, 
+                buffer
+            });
+            buffer.clear();
+
+            do { buffer.push_back(str[i]); ++i; }
+            while (
+                i < len && 
+                str[i] != '('  && 
+                str[i] != ' ' 
+            ); // Get the whole tag
+
+            if (i < len && str[i] == ' ')
+            {
+                buffer.push_back(' ');
+                wait_next_word = true;
+            }
+            else buffer.push_back('(');
+
+            tokens.push_back({ 
+                Token::Type::effect, 
+                buffer
+            });
+            buffer.clear();
+
+            if (i >= len)
+            {/* Silently close any open tags */ 
+                tokens.push_back({ 
+                    Token::Type::effect_stop, 
+                    ")"
+                });
+                break;
+            }
+            ++wait_right_parenthesis;
+            continue;
+        }
+        else
+        if (wait_right_parenthesis > 0 && c == ')')
+        {
+            --wait_right_parenthesis;
+
+            tokens.push_back({ 
+                Token::Type::text, 
+                buffer
+            });
+            buffer.clear();
+
+            tokens.push_back({ 
+                Token::Type::effect_stop, 
+                ")"
+            });
+            continue;
+        }
+        else
+        if (wait_next_word && c == ' ')
+        {
+            wait_next_word = false;
+            tokens.push_back({ 
+                Token::Type::text, 
+                buffer
+            });
+            buffer.clear();
+
+            tokens.push_back({ 
+                Token::Type::effect_stop, 
+                ")"
+            });
+
+            buffer.push_back(' ');
+            continue;
+        }
+        else buffer.push_back(c);
+    }
+
+    if (buffer.size() != 0)
+    {
+        tokens.push_back({ 
+            Token::Type::text, 
+            buffer
+        });
+        buffer.clear();
+    }
+    
+    if (wait_next_word) 
+    {
+        tokens.push_back({ 
+            Token::Type::effect_stop, 
+            ")"
+        });
+    }
+
+    return tokens;
+}
+
+std::list<Coltext::Node> 
+Coltext::parse (const std::list<Coltext::Token> &tokens) const
+{
+    using namespace ansi;
+
+    std::list<Node> nodes;
+    std::stack<Effect> effects;
+
+    std::stack<Effect> last_bg({Effect::default_bg});
+    std::stack<Effect> last_fg({Effect::default_fg});
+
+    bool ignore_stop = false;
+    for (const auto &tkn : tokens)
+    {
+        if (tkn.type == Token::Type::text)
+        {
+            nodes.push_back({
+                Node::Type::text, 
+                tkn.value
+            });
+            continue;
+        }
+        
+        Effect e;
+        if (tkn.type == Token::Type::effect)
+        {
+            std::string name = tkn.value;
+            name.pop_back(); // Delete ' ' or '('
+
+            if (name.at(0) == '#') name.erase(0, 1);
+
+            auto it = ansi::name_to_effect.find(name);
+            if (it == ansi::name_to_effect.end())
+            {
+            /* If it's not a valid effect, 
+                we just leave it as text */
+                nodes.push_back({
+                    Node::Type::text, 
+                    tkn.value
+                });
+                ignore_stop = true; 
+                continue;
+            }
+            else e = it->second;  
+
+            effects.push(e);
+            if ((e >= Effect::black_bg && e <= Effect::white_bg) ||
+                (e >= Effect::bright_black_bg && e <= Effect::bright_white_bg))
+            {
+                last_bg.push(e);
+            }
+            else
+            if ((e >= Effect::black_fg && e <= Effect::white_fg) ||
+                (e >= Effect::bright_black_fg && e <= Effect::bright_white_fg))
+            {
+                last_fg.push(e);
+            } 
+        }
+        else 
+        if (tkn.type == Token::Type::effect_stop)
+        {
+            if (ignore_stop)
+            {
+                ignore_stop = false;
+                continue;
+            }
+
+            e = effects.top();
+            effects.pop();
+            
+            if ((e >= Effect::black_bg && e <= Effect::white_bg) ||
+                (e >= Effect::bright_black_bg && e <= Effect::bright_white_bg))
+            {
+                last_bg.pop();
+                e = last_bg.top();
+            }
+            else
+            if ((e >= Effect::black_fg && e <= Effect::white_fg) ||
+                (e >= Effect::bright_black_fg && e <= Effect::bright_white_fg))
+            {
+                last_fg.pop();
+                e = last_fg.top();
+            } 
+            else e = effect_off.at(e);
+        }
+
+        // Create ANSI escape code from effect enum
+        std::string escape_code = "\033[" + std::to_string((int)e) + "m";
+
+        nodes.push_back({
+            Node::Type::effect, 
+            escape_code
+        });
+    }
+    return nodes;
+}
+
+#endif // COLTEXT_HPP
---
+++ tests.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+
+#include "coltext.hpp"
+
+namespace test {
+
+void plain_text() 
+{
+    std::cout << "Starting plain_text test:\n";
+
+    std::string msg = "Hello, world!";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test plain_text succeded\n\n";
+}
+
+void plain_text_with_parentheses()
+{
+    std::cout << "Starting plain_text_with_parentheses test:\n";
+
+    std::string msg = "Hello, world (and you)!";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test plain_text_with_parentheses succeded!\n\n";
+}
+
+void standard_escapes()
+{
+    std::cout << "Starting standard_escapes test:\n";
+
+    std::string msg = "Add tab here.\n\tAnd new lines!";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test standard_escapes succeded\n\n";
+}
+
+void color_with_parenthese()
+{
+    std::cout << "Starting color_with_parenthese test:\n";
+
+    std::string msg = "I love write black on #k(black!)";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test color_with_parenthese succeded\n\n"; 
+}
+
+void escaped_parenthese()
+{
+    std::cout << "Starting escaped_parenthese test:\n";
+
+    std::string msg = "#r(Text\\(With parenthese escaped\\)) is ok!";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test escaped_parenthese succeded\n\n";
+}
+
+void escaped_right_parenthese()
+{
+    std::cout << "Starting escaped_right_parenthese test:\n";
+
+    std::string msg = "#g(Text (With only right parenthese escaped\\)) is ok too!";
+    Coltext ctxt(msg);
+    
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test escaped_right_parenthese succeded\n\n";
+}
+
+void color_next_word() {
+    std::cout << "Starting color_next_word test:\n";
+
+    std::string msg = "Next #y word will be colored";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test color_next_word succeded\n\n"; 
+}
+
+void literal()
+{
+    std::cout << "Starting literal test:\n";
+
+    std::cout << "\t"  << "\"Text colored by #b(literal)\"_col" << "\n";
+    std::cout << "\t" << "Text colored by #b(literal)"_col << "\n";
+
+    std::cout << "[ OK ] Test literal succeded\n\n"; 
+}
+
+void bg_color()
+{
+    std::cout << "Starting bg_color test:\n";
+
+    std::string msg = "In order to color bg, you need to put color with #M(big letter)!";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test bg_color succeded\n\n"; 
+}
+
+void styles() {
+    std::cout << "Starting styles test:\n";
+
+    std::string msg1 = "<i> italic <b> bold <f> faint <u> underline";
+    std::string msg2 = "#<i> italic #<b> bold #<f> faint #<u> underline";
+    Coltext ctxt(msg1);
+
+    std::cout << "\t"  << msg1 << "\n";
+    std::cout << "\t"  << ctxt << "\n\n";
+    std::cout << "\t"  << msg2 << "\n";
+    std::cout << "\t"  << (Coltext) msg2 << "\n";
+
+    std::cout << "[ OK ] Test styles succeded\n\n"; 
+}
+
+void sequence()
+{
+    std::cout << "Starting sequence test:\n";
+
+    std::string msg = "#C(You #r(can) use much more than just 1 effect!)";
+    Coltext ctxt(msg);
+
+    std::cout << "\t"  << msg << "\n";
+    std::cout << "\t"  << ctxt << "\n";
+
+    std::cout << "[ OK ] Test sequence succeded\n\n"; 
+}
+
+void get_from_cin()
+{
+    std::cout << "Starting get_from_cin test:\n";
+
+    Coltext ctxt;
+    std::cout << "\t"; std::cin >> ctxt;
+    std::cout << "\t" << ctxt << "\n";
+
+    std::cout << "[ OK ] Test get_from_cin succeded\n\n"; 
+}
+
+void all_colors()
+{
+    std::cout
+        << "Foreground colors:\n"
+        << "\t\\#k(black)   -> " << "#k(black)\n"_col
+        << "\t\\#r(red)     -> " << "#r(red)\n"_col
+        << "\t\\#g(green)   -> " << "#g(green)\n"_col
+        << "\t\\#y(yellow)  -> " << "#y(yellow)\n"_col
+        << "\t\\#b(blue)    -> " << "#b(blue)\n"_col
+        << "\t\\#c(cyan)    -> " << "#c(cyan)\n"_col
+        << "\t\\#m(magenta) -> " << "#m(magenta)\n"_col
+        << "\t\\#w(white)   -> " << "#w(white)\n\n"_col
+
+        << "\t#bk(bright_black)   -> " << "#bk(bright_black)\n"_col
+        << "\t#br(bright_red)     -> " << "#br(bright_red)\n"_col
+        << "\t#bg(bright_green)   -> " << "#bg(bright_green)\n"_col
+        << "\t#by(bright_yellow)  -> " << "#by(bright_yellow)\n"_col
+        << "\t#bb(bright_blue)    -> " << "#bb(bright_blue)\n"_col
+        << "\t#bc(bright_cyan)    -> " << "#bc(bright_cyan)\n"_col
+        << "\t#bm(bright_magenta) -> " << "#bm(bright_magenta)\n"_col
+        << "\t#bw(bright_white)   -> " << "#bw(bright_white)\n\n"_col
+
+        << "Background colors:\n"
+        << "\t #K( ) -> " << "#K( )\n"_col
+        << "\t #R( ) -> " << "#R( )\n"_col
+        << "\t #G( ) -> " << "#G( )\n"_col
+        << "\t #Y( ) -> " << "#Y( )\n"_col
+        << "\t #B( ) -> " << "#B( )\n"_col
+        << "\t #C( ) -> " << "#C( )\n"_col
+        << "\t #M( ) -> " << "#M( )\n"_col
+        << "\t #W( ) -> " << "#W( )\n\n"_col
+
+        << "\t#bK( ) -> " << "#bK( )\n"_col
+        << "\t#bR( ) -> " << "#bR( )\n"_col
+        << "\t#bG( ) -> " << "#bG( )\n"_col
+        << "\t#bY( ) -> " << "#bY( )\n"_col
+        << "\t#bB( ) -> " << "#bB( )\n"_col
+        << "\t#bC( ) -> " << "#bC( )\n"_col
+        << "\t#bM( ) -> " << "#bM( )\n"_col
+        << "\t#bW( ) -> " << "#bW( )\n\n"_col;
+}
+
+} // namespace test
+
+int main(int argc, char const *argv[])
+{
+    /* Tests for not breaking standard functionality */
+    test::plain_text();
+    test::plain_text_with_parentheses();
+    test::standard_escapes();
+
+    test::color_with_parenthese();    // Does #xxx()# work ?
+    test::escaped_parenthese();       // Does #xxx(\\(Text in patenthese\\)) work ?
+    test::escaped_right_parenthese(); // Does #xxx((Text in patenthese\\)) work ?
+
+    test::color_next_word();       // Does #xxx next_word other_words work ?
+    test::styles();                // Does #<html_tag> work ?
+    test::literal();               // Does "text"_col work ?
+
+    test::bg_color();              // Does #Xxx work ?
+
+    test::sequence();              // Do sequences work ?
+
+    test::all_colors();
+
+    test::get_from_cin();          // Does operator>> work ?
+    return 0;
+}
